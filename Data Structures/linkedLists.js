12cc8f897e1fe5d7fc1eba24352ef6f8f991f37f
Julian <111262742+Marentxs@users.noreply.github.com>
2026-02-16 16:32:09 -0500

    add 4 additional methods to linkedlist class

--- Data Structures/linkedLists.js
+++ Data Structures/linkedLists.js
@@ -14,6 +14,43 @@ class LinkedList {
       this.tail = node;
     }
   }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.nextNode = this.head;
+      this.head = node;
+    }
+  }
+
+  size() {
+    let count = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      count += 1;
+      current = current.nextNode;
+    }
+
+    return count;
+  }
+
+  head() {
+    if (this.head === null) {
+      return undefined;
+    }
+    return this.head.value;
+  }
+
+  tail() {
+    if (this.head === null) {
+      return undefined;
+    }
+    return this.tail.value;
+  }
 }
 
 class Node {
