39f7e229c0d9ced817b588e6d35670b24e3bfa44
Julian <111262742+Marentxs@users.noreply.github.com>
2026-02-24 15:35:29 -0500

    add remove method

--- Data Structures/hashMap.js
+++ Data Structures/hashMap.js
@@ -76,4 +76,23 @@ class hashMap {
     }
     return false;
   }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    if (!bucket) {
+      return false;
+    }
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [existingKey, existingValue] = bucket[i];
+
+      if (existingKey === key) {
+        bucket.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
 }
