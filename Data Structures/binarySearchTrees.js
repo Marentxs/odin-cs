893f424bbdf9e771a15faa859a1e94d7e339fa71
Julian <111262742+Marentxs@users.noreply.github.com>
2026-03-02 16:38:56 -0500

    add buildTree method, with private function helper

--- Data Structures/binarySearchTrees.js
+++ Data Structures/binarySearchTrees.js
@@ -11,4 +11,25 @@ class Tree {
     this.arr = arr;
     this.root = this.buildTree(arr);
   }
+
+  #recursiveBST(arr, start, end) {
+    if (start > end) return null;
+
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(arr[mid]);
+
+    root.left = this.#recursiveBST(arr, start, mid - 1);
+    root.right = this.#recursiveBST(arr, mid + 1, end);
+
+    return root;
+  }
+
+  buildTree(arr) {
+    if (arr.length === 0) {
+      return null;
+    }
+    const newArr = [...new Set(arr)].sort((a, b) => a - b);
+
+    return this.#recursiveBST(newArr, 0, newArr.length - 1);
+  }
 }
